d5c0ca7189de7dfce12d13c4dd53c31af852c7eb
Math Peeters <pmathieu.peeters@gmail.com>
2026-04-09 19:05:49 +0200

    avancement

---
+++ GreenPilot.Api/Controllers/AuthControler.cs
@@ -0,0 +1,46 @@
+﻿using GreenPilot.Core.DTOs.RequestDtos.UserRequestDtos;
+using GreenPilot.Core.DTOs.ResponseDtos;
+using GreenPilot.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenPilot.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthControler(AuthService authService) : ControllerBase
+{
+  [HttpPost("Register")]
+  [EndpointSummary("Register")]
+  [EndpointDescription("Permet à un visiteur de s'enregistrer.")]
+  public async Task<ActionResult<AuthResponseDto>> Register(RegisterRequestDto dto)
+  {
+    try
+    {
+      AuthResponseDto result = await authService.RegisterAsync(dto);
+      return Ok(result);
+    }
+    catch (Exception e)
+    {
+      return BadRequest(e.Message);
+    }
+  }
+
+  [HttpPost("Login")]
+  [EndpointSummary("Login")]
+  [EndpointDescription("Permet à un membre de se logger")]
+  public async Task<ActionResult<AuthResponseDto>> Login(LoginRequestDto dto)
+  {
+    try
+    {
+      AuthResponseDto result = await authService.LoginAsync(dto);
+      return Ok(result);
+    }
+    catch (Exception e)
+    {
+      return BadRequest(e.Message);
+    }
+  }
+}
+
+
+
